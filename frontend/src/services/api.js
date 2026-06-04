235ff545fd41f951f74d4254a4c9e1450bf1bf00
Akshra-Tiwari <akshratiwari425@gmail.com>
2026-06-04 20:41:53 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-
-  baseURL:
-    "https://fds1-t172.onrender.com"
-
+  baseURL: "https://fds1-t172.onrender.com/api"
 });
 
 export default API;
